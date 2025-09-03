d6b4b9bf875a863099632a555b6c17afac5161dc
Aditya Pateriya <adityapateriya7986@gmail.com>
2025-09-03 21:51:47 +0530

    Create OddNumbers.java

---
+++ Week 1/Basics/OddNumbers.java
@@ -0,0 +1,12 @@
+package myProject;
+
+public class OddNumbers {
+	public static void main(String[] args) {
+		int i = 1;
+		while(i <= 100) {
+			if(i % 2 != 0)
+				System.out.print(i+" ");
+			i++;
+		}
+	}
+}
