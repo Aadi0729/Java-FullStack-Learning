e98fc225d3aef82046e463544e031e8d9469500b
Aditya Pateriya <adityapateriya7986@gmail.com>
2025-09-04 23:05:26 +0530

    Using method/function Factorial.java

---
+++ Week 1/Basics/FactorialNum.java
@@ -0,0 +1,25 @@
+package myProject;
+import java.util.Scanner;
+
+public class Factorial {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter the number: ");
+		int num = sc.nextInt();
+		
+		int prod = factorial(num);
+		
+		System.out.print("The factorial of "+num+" is "+prod);
+	}
+	
+	static int factorial(int n) {
+        int prod = 1;
+		
+		for(int i=n; i>1; i--) {
+			prod *= i;
+		}
+		
+		return prod;
+	}
+}
