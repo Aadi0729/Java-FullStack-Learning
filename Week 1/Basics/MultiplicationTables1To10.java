a705b158fbdfc8d2da6f04a966a068d3320a5ed4
Aditya Pateriya <adityapateriya7986@gmail.com>
2025-09-05 22:35:37 +0530

    Create MultiplicationTables1To10.java

---
+++ Week 1/Basics/MultiplicationTables1To10.java
@@ -0,0 +1,18 @@
+package myProject;
+
+public class MultiplicationTables1To10 {
+	
+	public static void main(String[] args) {
+		
+		int i = 1;
+		
+		while(i <= 10) {
+			
+			for(int j=1; j<=10; j++) {
+				System.out.print(i*j+" ");
+			}
+			System.out.println();
+			i++;
+		}
+	}
+}
