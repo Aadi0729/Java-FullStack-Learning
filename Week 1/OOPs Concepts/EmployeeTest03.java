ba9c34f4d7ae3e960af6af5896068f20a5c27e9d
Aditya Pateriya <adityapateriya7986@gmail.com>
2025-09-06 23:03:57 +0530

    Update EmployeeTest03.java

--- Week 1/OOPs Concepts/EmployeeTest03.java
+++ Week 1/OOPs Concepts/EmployeeTest03.java
@@ -6,11 +6,23 @@ class Employee {
 	int empNum;
 	String empName;
 	double empSalary;
+
+	/*
+	 * When we are assigning values directly to the data members inside a method,
+	 * this type of coding we call as HARD CODED DATA
+	 * 
+	 * When we go with HARD CODED DATA, How many objects we create, when we call
+	 * setEmpDetails() method. these values will remain same for all objects, which 
+	 * is not good in reality.
+	 * 
+	 * SOLUTION:
+	 * By sending data to the methods in the form of "arguments" -->> REFER EmployeeTest04.java
+	 */
 	
 	// Methods
 	void setEmpDetails() {
 		empNum = 101;
-		empName = "Rahul";
+		empName = "Rahul"; // HARD CODED DATA
 		empSalary = 150000.0;
 	}
 	
