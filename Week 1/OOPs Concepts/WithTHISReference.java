bbac1b1b9a2a048ed9ee7af6aa9a70c66b1af70c
Aditya Pateriya <adityapateriya7986@gmail.com>
2025-09-06 23:27:42 +0530

    Create WithTHISReference.java

---
+++ Week 1/OOPs Concepts/WithTHISReference.java
@@ -0,0 +1,35 @@
+package myProject;
+
+class Employee {
+	
+	// Data members
+	int empNum;
+	String empName;
+	double empSalary;
+	
+	// Methods
+	void setEmpDetails(int empNum, String empName, double empSalary) {
+		this.empNum = empNum;
+		this.empName = empName;
+		this.empSalary = empSalary;
+	}
+	
+	void displayEmpDetails() {
+		System.out.println("Employee Number= "+empNum);
+		System.out.println("Employee Name= "+empName);
+		System.out.println("Employee Salary= "+empSalary);
+	}
+}
+
+public class EmployeeTest {
+	public static void main(String[] args) {
+		
+		Employee e1 = new Employee();
+		e1.setEmpDetails(101, "Rahul", 150000.0);
+		e1.displayEmpDetails();
+		
+		Employee e2 = new Employee();
+		e2.setEmpDetails(102, "John", 140000.0);
+		e2.displayEmpDetails();
+	}
+}
