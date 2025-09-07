a0f38848402e7382fefdd078bdd1767652516b55
Aditya Pateriya <adityapateriya7986@gmail.com>
2025-09-07 13:54:31 +0530

    Create Program01 Using Constructor.java

---
+++ Week 1/OOPs Concepts/Program01 Using Constructor.java
@@ -0,0 +1,40 @@
+package myProject;
+
+class Student1 {
+	// Data members
+		int rollNum;
+		String stdName;
+		int mark1;
+		int mark2;
+		int mark3;
+		int totalMarks;
+		
+		// Parameterized Constructor
+		Student1 (int rollNum, String stdName, int mark1, int mark2, int mark3) {
+			this.rollNum = rollNum;
+			this.stdName = stdName;
+			this.mark1 = mark1;
+			this.mark2 = mark2;
+			this.mark3 = mark3;
+			
+			totalMarks = mark1 + mark2 + mark3;
+		}
+		
+		void displayStudDetails() {
+			System.out.println("Roll Number: "+rollNum);
+			System.out.println("Student Name: "+stdName);
+			System.out.println("Total Marks: "+totalMarks);
+		}
+}
+
+public class Program03 {
+	
+	public static void main(String[] args) {
+		Student1 s1 = new Student1(101, "Aditya", 97, 98, 87);
+		Student1 s2 = new Student1(102, "Rahul", 94, 86, 65);
+		
+		s1.displayStudDetails();
+		
+		s2.displayStudDetails();
+	}
+}
