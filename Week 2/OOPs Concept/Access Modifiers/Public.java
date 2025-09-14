0dfdb8fe1845110ecc9713d3ebc347cb0e8504ce
Aditya Pateriya <adityapateriya7986@gmail.com>
2025-09-14 19:51:45 +0530

    Create Public.java

---
+++ Week 2/OOPs Concept/Access Modifiers/Public.java
@@ -0,0 +1,17 @@
+public :
+
+1) A class can be public
+2) accessible everywhere
+3) If the class is public then the name of the source code file should be same as the classname
+
+Ex:
+MyClass.java
+public class MyClass {
+  
+}
+
+In a source code file, we can have only one public class
+Ex: 
+A.java (error)
+public class A {)
+public class B {)
