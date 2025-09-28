a42be9b73a3147ec14f9e2c8f321b335f7b76c61
Aditya Pateriya <adityapateriya7986@gmail.com>
2025-09-28 18:32:25 +0530

    Create ThreadDemo2.java

---
+++ Week 4/ThreadDemo2.java
@@ -0,0 +1,28 @@
+package thread;
+
+class MyThread extends Thread {
+	
+	public void run() {
+		// System.out.println("I am a thread");
+		Thread t = Thread.currentThread();
+		System.out.println(t);
+	}
+}
+
+public class ThreadMethodsDemo {
+	public static void main(String[] args) {
+		
+		MyThread t = new MyThread();
+		t.run();
+	}
+}
+
+/* That's not a MultiThread program. When we run the program, the first thread which it creates is "main".
+ 
+   OUTPUT:
+   Thread[#3,main,5,main]
+   
+   Until the run() method is not completed, it won't come out of it. So its a ordinary program.
+   
+   Thread can only be created through start(). Without start() there is no concept of thread.
+*/
