b1a73a0f15ac3d02ae52ed1dd6092c76931be264
Aditya Pateriya <adityapateriya7986@gmail.com>
2025-09-28 21:51:52 +0530

    Create ArrayList.java

---
+++ Week 4/Collections/ArrayList.java
@@ -0,0 +1,27 @@
+package collections;
+import java.util.*;
+
+public class ArrayListDemo {
+	
+	public static void main(String[] args) {
+		
+		ArrayList al = new ArrayList();
+		al.add(10);
+		al.add(20);
+		al.add("ppp");
+		al.add("qqq");
+		al.add(10);
+		System.out.println(al); //[10, 20, ppp, qqq, 10]
+		al.add(2, "xxx");
+		System.out.println(al); //[10, 20, xxx, ppp, qqq, 10]
+		al.set(2, "yyy");
+		System.out.println(al); //[10, 20, yyy, ppp, qqq, 10]
+		al.remove("yyy");
+		System.out.println(al); //[10, 20, ppp,qqq, 10]
+		al.remove(2);
+		System.out.println(al); //[10,20,qqq, 10]
+		String s = (String) al.get(2);
+		System.out.println(s); //qqq
+		System.out.println("Size = "+al.size()); //Size = 4
+	}
+}
