2e32160a1d33e0f7670fab25ed6e2d200826ea39
Aditya Pateriya <adityapateriya7986@gmail.com>
2025-12-18 15:16:18 +0530

    Bank class with user information method

---
+++ 100-Days-Java-Backend/Day 03/ClassesDemo/Bank.java
@@ -0,0 +1,11 @@
+package Day03.classesDemo;
+
+public class Bank {
+    String userName;
+    int userAge;
+    String address;
+
+    public void info() {
+        System.out.println(userName + " age is " + userAge + " and lives in " + address);
+    }
+}
