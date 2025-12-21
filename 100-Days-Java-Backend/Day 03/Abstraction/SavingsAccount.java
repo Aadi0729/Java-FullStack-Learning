9b689835f434c47a913fd8d082e9090e386901b9
Aditya Pateriya <adityapateriya7986@gmail.com>
2025-12-21 15:32:42 +0530

    SavingsAccount.java - SubClass

--- 100-Days-Java-Backend/Day 03/Abstraction/SavingsAccount.java
+++ 100-Days-Java-Backend/Day 03/Abstraction/SavingsAccount.java
@@ -1,6 +1,11 @@
 package Day03.abstraction;
 
 public class SavingsAccount extends BankAccount {
+
+    public SavingsAccount(String accountNumber, double balance) {
+        super(accountNumber, balance);
+    }
+
     @Override
     public void deposit(double amount) {
         System.out.println("Deposited");
