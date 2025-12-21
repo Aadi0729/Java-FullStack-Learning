6fbc779e5c27fa5598816879d212c675a332175d
Aditya Pateriya <adityapateriya7986@gmail.com>
2025-12-21 15:32:09 +0530

    CurrentAccount.java - SubClass

--- 100-Days-Java-Backend/Day 03/Abstraction/CurrentAccount.java
+++ 100-Days-Java-Backend/Day 03/Abstraction/CurrentAccount.java
@@ -1,13 +1,17 @@
 package Day03.abstraction;
 
 public class CurrentAccount extends BankAccount {
+    public CurrentAccount(String accountNumber, double balance) {
+        super(accountNumber, balance);
+    }
+
     @Override
     public void deposit(double amount) {
-        System.out.println("Deposited");
+        System.out.println("Deposited Current");
     }
 
     @Override
     public void withdraw(double amount) {
-        System.out.println("Withdrawn");
+        System.out.println("Withdrawn Current");
     }
 }
