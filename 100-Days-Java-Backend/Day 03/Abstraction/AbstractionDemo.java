eb9ea600cd4b38503c4d9cd94fa990e589f0b07b
Aditya Pateriya <adityapateriya7986@gmail.com>
2025-12-21 15:30:52 +0530

    AbstractionDemo.java

--- 100-Days-Java-Backend/Day 03/Abstraction/AbstractionDemo.java
+++ 100-Days-Java-Backend/Day 03/Abstraction/AbstractionDemo.java
@@ -3,11 +3,14 @@ package Day03.abstraction;
 public class AbstractionDemo {
     public static void main(String[] args) {
 
-        SavingsAccount savingsAccount = new SavingsAccount();
+        SavingsAccount savingsAccount = new SavingsAccount("hfhnbnsnsnd", 60000.0);
         savingsAccount.deposit(100);
 
-        BankAccount savingsAccount1 = new SavingsAccount();
-        savingsAccount1 = new CurrentAccount();
+        BankAccount savingsAccount1 = new SavingsAccount("hfhnbnsnsnd", 60000.0);
+        savingsAccount1.deposit(300);
+        savingsAccount1 = new CurrentAccount("ahdjrdjvjne", 10000.0);
         savingsAccount1.deposit(200);
+
+        System.out.println(savingsAccount1.getAccountNumber());
     }
 }
