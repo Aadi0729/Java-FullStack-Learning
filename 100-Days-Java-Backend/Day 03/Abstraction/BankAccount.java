843e0176a5577cbe38d2263c96bf7ef477723928
Aditya Pateriya <adityapateriya7986@gmail.com>
2025-12-21 15:31:38 +0530

    BankAccount.java - Parent Class

--- 100-Days-Java-Backend/Day 03/Abstraction/BankAccount.java
+++ 100-Days-Java-Backend/Day 03/Abstraction/BankAccount.java
@@ -8,4 +8,25 @@ abstract public class BankAccount {
     // No need of implementation here
     public abstract void deposit(double amount);
     public abstract void withdraw(double amount);
+
+    public BankAccount(String accountNumber, double balance) {
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
 }
