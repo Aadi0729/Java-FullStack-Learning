56cf3f78c240b29a9cd9ebb0d25bb04c688804ba
Aditya Pateriya <adityapateriya7986@gmail.com>
2026-01-13 23:41:05 +0530

    UserManager.java

---
+++ 100-Days-Java-Backend/Day 10/Challenge - Implementing IOC using Spring Container/UserManager.java
@@ -0,0 +1,17 @@
+package ioc.loose.coupling;
+
+public class UserManager {
+    /*
+    Here UserManager is just dependent on interface, and whoever implements that interface can make use
+    of UserManager
+    */
+    private final UserDataProvider userDataProvider;
+
+    public UserManager(UserDataProvider userDataProvider) {
+        this.userDataProvider = userDataProvider;
+    }
+
+    public String getUserInfo() {
+        return userDataProvider.getUserDetails();
+    }
+}
