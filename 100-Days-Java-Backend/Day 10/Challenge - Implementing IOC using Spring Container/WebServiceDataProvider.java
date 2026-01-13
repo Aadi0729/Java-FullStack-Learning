074dbaaa6a43653ef413910fe5ed753c6198abfd
Aditya Pateriya <adityapateriya7986@gmail.com>
2026-01-13 23:43:07 +0530

    WebServiceDataProvider.java

---
+++ 100-Days-Java-Backend/Day 10/Challenge - Implementing IOC using Spring Container/WebServiceDataProvider.java
@@ -0,0 +1,9 @@
+package ioc.loose.coupling;
+
+public class WebServiceDataProvider implements UserDataProvider {
+
+        @Override
+        public String getUserDetails() {
+            return "Fetching Data From Web Service";
+        }
+}
