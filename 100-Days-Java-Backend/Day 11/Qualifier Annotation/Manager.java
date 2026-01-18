281c2a22c3b086d3499f6de43cf8cd94fa6873de
Aditya Pateriya <adityapateriya7986@gmail.com>
2026-01-18 18:33:40 +0530

    Manager.java

---
+++ 100-Days-Java-Backend/Day 11/Qualifier Annotation/Manager.java
@@ -0,0 +1,25 @@
+package com.example.qualiferAnnotation;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Manager {
+    @Autowired
+    @Qualifier("employee")
+    private Employee employee;
+
+    @Autowired
+    /*@Qualifier // Qualifier is NOT applicable to Constructor*/
+    public Manager(Employee employee) {
+        this.employee = employee;
+    }
+
+    @Override
+    public String toString() {
+        return "Manager{" +
+                "employee=" + employee +
+                '}';
+    }
+}
