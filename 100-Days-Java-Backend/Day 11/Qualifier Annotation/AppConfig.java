29434405ca55939783866779ec9cb271cfbfe0f1
Aditya Pateriya <adityapateriya7986@gmail.com>
2026-01-18 18:34:58 +0530

    AppConfig.java

---
+++ 100-Days-Java-Backend/Day 11/Qualifier Annotation/AppConfig.java
@@ -0,0 +1,9 @@
+package com.example.qualiferAnnotation;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.example.qualiferAnnotation")
+public class AppConfig {
+}
