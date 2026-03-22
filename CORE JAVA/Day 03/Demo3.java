6d0298bba35d11fd9b0659e25c730543d76d7867
Aditya Pateriya <adityapateriya7986@gmail.com>
2026-03-22 23:50:37 +0530

    Array & Strings

---
+++ CORE JAVA/Day 03/Demo3.java
@@ -0,0 +1,11 @@
+public class Demo3 {
+    public static void main(String[] args) {
+        // Strings
+        String firstName = "Aditya";
+        String lastName = "Pateriya";
+
+        String fullName = firstName + " " + lastName;
+
+        System.out.println(fullName);  // Aditya Pateriya
+    }
+}
